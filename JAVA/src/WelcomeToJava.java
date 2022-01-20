7134ad3c724276b306f3d549d919c3fcd75b8023
zurijang <wkdwn05@gmail.com>
2022-01-21 01:18:29 +0900

    01. [STDOUT] Welcome To Java

---
+++ JAVA/src/WelcomeToJava.java
@@ -0,0 +1,15 @@
+/*
+ * Solution Class와 main method를 구현
+ * stdout
+ */
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+    }
+
+}
